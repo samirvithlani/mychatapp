f9b83568064d7513e696d357fecbff9fb953cfa6
=samirvithlani <=samir.vithlani83955@gmail.com>
2021-01-26 17:02:43 +0530

    rendering

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,47 @@
 const socket = io()
 
-socket.on('message',(message)=>{
+//elemets
+const $messagForm = document.querySelector('#message-form')
+const $messagFormInput = document.querySelector('input')
+const $messagFormButton = document.querySelector('button')
+const $messages = document.querySelector('#messages')
+const messagTemplate = document.querySelector('#message-template').innerHTML
+socket.on('message', (message) => {
+
     console.log(message)
+    const html = Mustache.render(messagTemplate,{
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+    
 })
-document.querySelector('#message-form').addEventListener('submit',(e)=>{
+document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     e.preventDefault()
+    //disbale
+    $messagFormButton.setAttribute('disabled','disabled')
+
     const message = e.target.elements.message.value
-    socket.emit('sendMessage',message)
+    socket.emit('sendMessage', message, () => {
+        //enable
+        $messagFormButton.removeAttribute('disabled')
+        $messagFormInput.value=''
+        $messagFormInput.focus()
+        console.log("message delivered..")
+    })
 })
 
-document.querySelector('#send-location').addEventListener('click',()=>{
+document.querySelector('#send-location').addEventListener('click', () => {
 
-    if(!navigator.geolocation){
+    if (!navigator.geolocation) {
         return alert('locatioin is not supported by browser')
     }
-    navigator.geolocation.getCurrentPosition((position)=>{
+    navigator.geolocation.getCurrentPosition((position) => {
 
         console.log(position)
-        socket.emit('sendLocation',{
-            latitude:position.coords.latitude,
-            longitude:position.coords.longitude
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
 
         })
     })
--- src/index.js
+++ src/index.js
@@ -17,8 +17,9 @@ io.on('connection', (socket) => {
 
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message','new user joined')
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message,callback) => {
         io.emit('message', message)
+        callback()
     })
     socket.on('sendLocation',(cords)=>{
         //io.emit('message',`Location:${cords.latitude},${cords.longitude}`)
